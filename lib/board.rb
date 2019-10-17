ea41f8ddc8b10696ab1ff98a980fa84aeca110c4
R David N H <rdavidnh@gmail.com>
2019-10-17 16:19:58 +0300

    updated files

--- lib/board.rb
+++ lib/board.rb
@@ -2,37 +2,70 @@ require_relative 'board_case'
 require_relative 'show'
 
 class Board
+  # array of board cases
   attr_accessor :cases
   
   def initialize
     @cases = []
     
+    # set position of cases
     pos = [
       'A1', 'A2', 'A3',
       'B1', 'B2', 'B3',
       'C1', 'C2', 'C3'
     ]
     
+    # create 9 cases
     9.times do |i|
       @cases << BoardCase.new(' ', pos[i])
     end
   end
   
   def play_turn(player)
+    # ask a player for a command on his turn
     puts "C'est le tour de " + player.name + "!"
     print "-> "
-    commande = gets.chomp
+    command = gets.chomp
     
+    # search for the selected case and replace its value if it is empty
     @cases.each do |bcase|
-      if bcase.pos == commande
+      if bcase.pos == command && bcase.value == ' '
         bcase.value = player.symbole
+      elsif bcase.pos == command && bcase.value != ' '
+        puts '!!!!' * 12
+        puts "Case déjà utilisée, choisissez une autre case!"
+        puts '!!!!' * 12
       end
     end
     
   end
   
-  def victory?
+  def victory?(player)
     
+    winner = [
+      # horizontal
+      @cases[0].value == player.symbole && @cases[1].value == player.symbole && @cases[2].value == player.symbole,
+      @cases[3].value == player.symbole && @cases[4].value == player.symbole && @cases[5].value == player.symbole,
+      @cases[6].value == player.symbole && @cases[7].value == player.symbole && @cases[8].value == player.symbole,
+      # vertical
+      @cases[0].value == player.symbole && @cases[3].value == player.symbole && @cases[6].value == player.symbole,
+      @cases[1].value == player.symbole && @cases[4].value == player.symbole && @cases[7].value == player.symbole,
+      @cases[2].value == player.symbole && @cases[5].value == player.symbole && @cases[8].value == player.symbole,
+      # diagonal
+      @cases[0].value == player.symbole && @cases[4].value == player.symbole && @cases[8].value == player.symbole,
+      @cases[2].value == player.symbole && @cases[4].value == player.symbole && @cases[6].value == player.symbole
+    ]
+    
+    # test all the winner combination
+    # return true if there is a winner
+    winner.each do |win|
+      if win
+        return true
+      end
+    end
+    
+    # return false if no winner
+    return false
   end
   
 end
--- lib/game.rb
+++ lib/game.rb
@@ -8,8 +8,11 @@ class Game
 
   def initialize
     @players = []
+    @status = "on going"
+    @board = Board.new
     @counter = 0
     
+    # ask for the players names
     puts "Entrez nom du joueur 1"
     name1 = gets.chomp
     puts "Entrez nom du joueur 2"
@@ -18,29 +21,42 @@ class Game
     @players << Player.new(name1, 'X')
     @players << Player.new(name2, 'O')
     
-    @status = "on going"
-    @board = Board.new
+    # set the current player to player 1
     @current_player = @players[0]
     
     puts "=" * 25
     puts "Commencez!!!!!"
+    # display the board for the first time
     Show.new.show_board(@board)
   end
   
   def turn
-    
+    # start the game with the current player
     @board.play_turn(@current_player)
     
+    # display current turn number
+    puts "Tour ##{@counter + 1}"
+    
+    # display the board every turn
+    Show.new.show_board(@board)
+    
+    # check if the current player has won
+    if @board.victory?(@current_player)
+      puts "-" * 30
+      puts @current_player.name + " IS THE WINNEEEER!!!"
+      puts "*" * 30
+      # set game status to finished
+      @status = "finished"
+    end
+    
+    # change player after each turn
     if @current_player == @players[0]
       @current_player = @players[1]
     elsif @current_player = @players[1]
       @current_player = @players[0]
     end
     
-    puts "Tour #" + (@counter + 1).to_s
-    
-    Show.new.show_board(@board)
-    
+    # count the turn
     @counter += 1
   end
   
@@ -50,11 +66,21 @@ class Game
   end
   
   def game_end
+    puts "=" * 35
     puts "Partie terminéeee!!!"
-    
+    puts "MATCH NUL!" unless @board.victory?(@current_player) 
+    puts "*" * 35
     puts "Que voulez vous faire?"
-    puts "-" * 35
+    puts "1 - Rejouer :D"
+    puts "2 - Nouveau jeu"
+    puts "3 - Quitter :("
     
+    cmd = gets.chomp
+    
+    if cmd == "3"
+      @status = "quit"
+      puts "Byee byeeee!!!!"
+    end
     
   end
 
